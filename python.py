5dfcec466cfb13d6d929beb23f66b79afd7af6cb
Mona567 <monajaiswal1192000@gmail.com>
2024-12-07 23:33:25 +0530

    added code in python.py

--- python.py
+++ python.py
@@ -1,5 +1,9 @@
 import mysql.connector as c
-con=c.connect(host="localhost",
-              user="root",
-              passwd="Mysql@.11",
-              database="Covid_Vaccination_System")
+
+def connect_to_db():
+    return c.connect(
+        host="localhost",       # Your MySQL host
+        user="root",            # Your MySQL username
+        passwd="Mysql@.11",     # Your MySQL password
+        database="Covid_Vaccination_System"  # Your database name
+    )
