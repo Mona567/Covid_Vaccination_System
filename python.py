4120f81876d2583eb6820969cdab7f0e805c267d
Mona567 <monajaiswal1192000@gmail.com>
2024-12-09 01:44:19 +0530

    added python.py

---
+++ python.py
@@ -0,0 +1,14 @@
+import mysql.connector as c
+
+def connect_to_db():
+    return c.connect(
+        host="localhost",       
+        user="root",
+        passwd="Mysql@.11",    
+        database="Covid_Vaccination_System"  
+    )
+
+if con.is_connected():
+    print(True)
+
+
